fcd799ca4d488a35f994fb8b1b8f7e33f488193b
Saber Tabatabaee Yazdi <saber.tabatabaee@gmail.com>
2023-03-25 18:09:10 +0330

    can share all post in virgool

--- common.js
+++ common.js
@@ -1,33 +1,32 @@
 function createMyExtensionDivision(id, className) {
     //Creating Elements
-    var div = document.createElement("div")
+    var div = document.createElement("div");
     div.id = id;
     if (className) {
         div.classList.add(className);
     }
     return div;
-};
+}
 
 function createButton(id, text) {
     //Creating Elements
-    var btn = document.createElement("button")
+    var btn = document.createElement("button");
     btn.id = id;
     var t = document.createTextNode(text);
     btn.appendChild(t);
     return btn;
-};
+}
 
 function createTextBox(id, text) {
     //Creating Elements
-    var textBox = document.createElement("INPUT")
+    var textBox = document.createElement("INPUT");
     textBox.id = id;
     textBox.setAttribute("type", "text");
     return textBox;
-};
-
+}
 
 function checkUrl(url) {
-    var testLoc = document.createElement('a');
+    var testLoc = document.createElement("a");
     testLoc.href = url.toLowerCase();
     current_url = testLoc.hostname;
     if (current_url.indexOf(url) !== -1) {
@@ -36,22 +35,17 @@ function checkUrl(url) {
     return false;
 }
 
-
 function getQuerySearchPhraseGoogleDotCom() {
     return (function (a) {
-        if (a == "")
-            return {};
+        if (a == "") return {};
         var b = {};
         for (var i = 0; i < a.length; ++i) {
-            var p = a[i].split('=', 2);
-            if (p.length == 1)
-                b[p[0]] = "";
-
-            else
-                b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+            var p = a[i].split("=", 2);
+            if (p.length == 1) b[p[0]] = "";
+            else b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
         }
         return b;
-    })(window.location.search.substr(1).split('&'));
+    })(window.location.search.substr(1).split("&"));
 }
 
 function saveToken() {
@@ -62,11 +56,14 @@ function saveToken() {
 }
 
 function saveInChromeStorageByKey(key, value) {
-    chrome.storage.sync.set({
-        'berimbasket_token': value
-    }, function () {
-        console.log('Settings saved');
-    });
+    chrome.storage.sync.set(
+        {
+            berimbasket_token: value,
+        },
+        function () {
+            console.log("Settings saved");
+        }
+    );
 }
 
 function addMyExtensionDivToPage(elementId, AfterElementId, className) {
@@ -91,17 +88,21 @@ function addTextBoxByElementId(elementId, AfterElementId, buttonText) {
     return textbox;
 }
 
-function call_virgool_share_api(element, token) {
-    document.getElementById("share_virgool_btn").setAttribute("disabled", "");
+function call_virgool_share_api(element, token, elementId) {
+    document.getElementById(elementId).setAttribute("disabled", "");
 
     // document.querySelector("#app").style.backgroundColor = 'black';
     var request = new XMLHttpRequest();
     console.log(element.getAttribute("href"));
 
     // Open a new connection, using the GET request on the URL endpoint
-    var url = "https://berimbasket.ir/bball/bots/resend.php?token=" + token + "&" + element.getAttribute("href");
+    var url =
+        "https://berimbasket.ir/bball/bots/resend.php?token=" +
+        token +
+        "&" +
+        element.getAttribute("href");
     console.log(url);
-    request.open('GET', url, true)
+    request.open("GET", url, true);
 
     request.onload = function () {
         // Begin accessing JSON data here
@@ -109,20 +110,19 @@ function call_virgool_share_api(element, token) {
         console.log(this); // 'this' should be a XMLHttpRequest object
         console.log(this.status);
         if (this.status == 200) {
-            var btn = document.getElementById("share_virgool_btn");
+            var btn = document.getElementById(elementId);
             btn.innerText = "Done";
         } else {
-            document.getElementById("share_virgool_btn").removeAttribute("disabled");
+            document.getElementById(elementId).removeAttribute("disabled");
         }
         console.log(this.responseText);
         if (this.responseText == "token not correct") {
             addSaveTokenDivToPage();
-
         }
-    }
+    };
 
     // Send request
-    request.send()
+    request.send();
 }
 
 function call_google_share_api(element, token) {
@@ -132,9 +132,13 @@ function call_google_share_api(element, token) {
     var request = new XMLHttpRequest();
     console.log(element.getAttribute("href"));
     // Open a new connection, using the GET request on the URL endpoint
-    var url = "https://berimbasket.ir/bball/bots/resend.php?token=" + token + "&" + element.getAttribute("href");
+    var url =
+        "https://berimbasket.ir/bball/bots/resend.php?token=" +
+        token +
+        "&" +
+        element.getAttribute("href");
     console.log(url);
-    request.open('GET', url, true)
+    request.open("GET", url, true);
 
     request.onload = function () {
         // Begin accessing JSON data here
@@ -145,26 +149,34 @@ function call_google_share_api(element, token) {
             var btn = document.getElementById("share_google_btn");
             btn.innerText = "Done";
         } else {
-            document.getElementById("share_google_btn").removeAttribute("disabled");
+            document
+                .getElementById("share_google_btn")
+                .removeAttribute("disabled");
         }
         console.log(this.responseText);
         if (this.responseText == "token not correct") {
             addSaveTokenDivToPage();
-
         }
-    }
+    };
 
     // Send request
-    request.send()
+    request.send();
 }
 
 function addSaveTokenDivToPage() {
-    var button = addButtonByElementId("save_token_btn", "share_google_div", "save");
-    var textbox = addTextBoxByElementId("berimbasket_token_txt", "share_google_div", "save");
-    button.addEventListener('click', () => saveToken());
+    var button = addButtonByElementId(
+        "save_token_btn",
+        "share_google_div",
+        "save"
+    );
+    var textbox = addTextBoxByElementId(
+        "berimbasket_token_txt",
+        "share_google_div",
+        "save"
+    );
+    button.addEventListener("click", () => saveToken());
 }
 
-
 function getSecondPart(str) {
-    return str.split('https://vrgl.ir/')[1];
+    return str.split("https://vrgl.ir/")[1];
 }
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 window.onload = () => {
-    var google = 'google.com';
-    var virgool = 'virgool.io';
-    var youtube = 'youtube.com';
-    var aparat = 'aparat.com';
-    var stackoverflow = 'stackoverflow.com';
+    var google = "google.com";
+    var virgool = "virgool.io";
+    var youtube = "youtube.com";
+    var aparat = "aparat.com";
+    var stackoverflow = "stackoverflow.com";
 
     if (checkUrl(google)) {
         console.log(google);
@@ -14,33 +14,48 @@ window.onload = () => {
             AfterElementId = "hdr";
         }
 
-        var extension_div = addMyExtensionDivToPage(parentElementId, AfterElementId, "");
+        var extension_div = addMyExtensionDivToPage(
+            parentElementId,
+            AfterElementId,
+            ""
+        );
 
         if (extension_div) {
             elementId = "share_google_btn";
-            var button = addButtonByElementId(elementId, extension_div.id, "Share");
+            var button = addButtonByElementId(
+                elementId,
+                extension_div.id,
+                "Share"
+            );
 
-            console.log(google + ':3:' + elementId);
+            console.log(google + ":3:" + elementId);
 
             var token = "";
             // Read it using the storage API
-            chrome.storage.sync.get(['berimbasket_token'], function (items) {
+            chrome.storage.sync.get(["berimbasket_token"], function (items) {
                 // message('Settings retrieved', items);
-                console.log('Settings retrieved : ' + items['berimbasket_token']);
-                token = items['berimbasket_token'];
+                console.log(
+                    "Settings retrieved : " + items["berimbasket_token"]
+                );
+                token = items["berimbasket_token"];
 
                 if (token) {
                     var queryString = getQuerySearchPhraseGoogleDotCom();
                     var query = queryString["q"];
                     var button = document.getElementById("share_google_btn");
-                    button.setAttribute("href", "q=" + query + "&origin=" + google);
-                    button.addEventListener('click', () => call_google_share_api(button, token));
+                    button.setAttribute(
+                        "href",
+                        "q=" + query + "&origin=" + google
+                    );
+                    button.addEventListener("click", () =>
+                        call_google_share_api(button, token)
+                    );
                 } else {
                     addSaveTokenDivToPage();
                 }
             });
         } else {
-            console.log("parent div not found!")
+            console.log("parent div not found!");
         }
     } else if (checkUrl(youtube)) {
         console.log(youtube);
@@ -48,37 +63,102 @@ window.onload = () => {
         console.log(aparat);
     } else if (checkUrl(stackoverflow)) {
         console.log(stackoverflow);
-        
     } else if (checkUrl(virgool)) {
         console.log(virgool);
-        AfterElementId = "react-app";
-        parentElementId = "share_virgool_div";
 
-        var extension_div = addMyExtensionDivToPage(parentElementId, AfterElementId, "_3a4R8tvUs3FQ9jdg7-p6iu");
-        if (extension_div) {
-            elementId = "share_virgool_btn";
-            var button = addButtonByElementId(elementId, extension_div.id, "Share");
+        elementId = "share_virgool_btn";
 
-            console.log(google + ':3:' + elementId);
+        if (document.getElementById(elementId) != null) {
+            AfterElementId = "react-app";
+            parentElementId = "share_virgool_div";
+
+            var extension_div = addMyExtensionDivToPage(
+                parentElementId,
+                AfterElementId,
+                "_3a4R8tvUs3FQ9jdg7-p6iu"
+            );
+
+            var button = addButtonByElementId(
+                elementId,
+                extension_div.id,
+                "Share"
+            );
+
+            console.log(google + ":3:" + elementId);
 
             var token = "";
             // Read it using the storage API
-            chrome.storage.sync.get(['berimbasket_token'], function (items) {
+            chrome.storage.sync.get(["berimbasket_token"], function (items) {
                 // message('Settings retrieved', items);
-                console.log('Settings retrieved : ' + items['berimbasket_token']);
-                token = items['berimbasket_token'];
+                console.log(
+                    "Settings retrieved : " + items["berimbasket_token"]
+                );
+                token = items["berimbasket_token"];
 
                 if (token) {
-                    var share_post__share_link_copy_input = document.getElementById("share-post--share-link-copy-input");
+                    var share_post__share_link_copy_input =
+                        document.getElementById(
+                            "share-post--share-link-copy-input"
+                        );
                     var url = share_post__share_link_copy_input.value;
                     url = getSecondPart(url);
-                    var button = document.getElementById("share_virgool_btn");
-                    button.setAttribute("href", "url=" + url + "&origin=" + virgool);
-                    button.addEventListener('click', () => call_virgool_share_api(button, token));
+                    var button = document.getElementById(elementId);
+                    button.setAttribute(
+                        "href",
+                        "url=" + url + "&origin=" + virgool
+                    );
+                    button.addEventListener("click", () =>
+                        call_virgool_share_api(button, token, elementId)
+                    );
+                } else {
+                    addSaveTokenDivToPage();
+                }
+            });
+        }
+
+        if (document.getElementsByClassName("share-buttons") != null) {
+            elementId = "share-buttons";
+
+            AfterElementId = "app-root-single-ads";
+            parentElementId = "share_virgool_div";
+
+            var extension_div = addMyExtensionDivToPage(
+                parentElementId,
+                AfterElementId,
+                "_3a4R8tvUs3FQ9jdg7-p6iu"
+            );
+
+            var button = addButtonByElementId(
+                elementId,
+                extension_div.id,
+                "Share"
+            );
+            var token = "";
+            // Read it using the storage API
+            chrome.storage.sync.get(["berimbasket_token"], function (items) {
+                // message('Settings retrieved', items);
+                console.log(
+                    "Settings retrieved : " + items["berimbasket_token"]
+                );
+                token = items["berimbasket_token"];
+
+                if (token) {
+                    var url =
+                        document.getElementsByClassName("share-buttons")[0]
+                            .innerText;
+                    url = getSecondPart(url);
+                    var button = document.getElementById(elementId);
+                    button.setAttribute(
+                        "href",
+                        "url=" + url + "&origin=" + virgool
+                    );
+                    button.addEventListener("click", () =>
+                        call_virgool_share_api(button, token, elementId)
+                    );
                 } else {
                     addSaveTokenDivToPage();
                 }
             });
         }
     }
-}
+};
